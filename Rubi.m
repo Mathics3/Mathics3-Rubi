5b77deff3a332e4e21d005734f063a611d609778
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2026-01-07 22:23:00 +0000

    Intermediate fix for CheckArguments

--- Rubi.m
+++ Rubi.m
@@ -217,11 +217,6 @@ TraceBuiltins[dvFixWithNoCond = RuleDelayed[Verbatim[HoldPattern][lhs_],With[var
 TraceBuiltins[DownValues[Int] = DownValues[Int] /. dvFixWith /. dvFixWithNoCond, SortBy->"count"];
 Print[""];
 
-(* Dummy CheckArguments *)
-CheckArguments[a_, n_] := n == 1;
-CheckArguments[f_[args___], n_] := Length[{args}] == n;
-SetAttributes[CheckArguments, HoldFirst];
-
 If[$LoadShowSteps === True,
   StatusBarPrint["Modifying " <> ToString[$RuleCount] <> " integration rules to display steps..."];
   StepFunction[Int];
